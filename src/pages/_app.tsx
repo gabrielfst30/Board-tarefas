7696d091d1596ccdb05535412c999e687e598b7d
gabrielfst30 <gabrielfst30@gmail.com>
2023-06-23 19:24:12 -0300

    feature: session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import Header from '@/components/Header'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header/>
+    // Importando o SessionProvider do NextAuth para autenticaçao social
+    <SessionProvider session={pageProps.session}>
+      <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
