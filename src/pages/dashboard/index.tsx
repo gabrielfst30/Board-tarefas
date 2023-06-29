dd4e98a50a6061bad7eea00223a0a0d861a0a7d4
gabrielfst30 <gabrielfst30@gmail.com>
2023-06-29 14:17:00 -0300

    feature:  server-side session

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,10 @@
+import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
+
+//pegando a sessão do usuário
+import { getSession } from 'next-auth/react';
+import { TextArea } from '@/components/TextArea';
 
 export default function Dashboard(){
     return(
@@ -7,8 +12,46 @@ export default function Dashboard(){
             <Head>
                 <title>Meu painel de tarefas</title>
             </Head>
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        <h1 className={styles.title}>Qual sua tarefa?</h1>
+                        <form>
+                            <TextArea
+                                placeholder='Digite qual sua tarefa..'
+                            />
+                            <div className={styles.checkboxArea}>
+                                <input type="checkbox" className={styles.checkbox}/>
+                                <label>Deixar tarefa pública?</label>    
+                            </div>
 
-            <h1>Pagina Painel</h1>
+                            <button className={styles.button} type='submit'>
+                                Registrar
+                            </button>
+                        </form>
+                    </div>
+                </section>
+            </main>
         </div>
     )
+}
+
+//server side com tipagem para melhorar a intellisense
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({ req });
+   // console.log(session);
+
+   if(!session?.user){
+    //se não tiver sessão do usuário vamos direcionar para o home
+        return{
+            redirect:{
+                destination:'/',
+                permanent: false,
+            }
+        }
+   }
+    
+    return {
+        props:{}
+    }
 }
